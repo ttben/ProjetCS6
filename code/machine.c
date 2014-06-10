9076084771ba837ef0b89a895d8ff64856fcc17a
benjamin BENNI <bb102748@z003.(none)>
2014-06-10 08:53:08 +0200

    Modification du ignore

---
+++ code/machine.c
@@ -0,0 +1,38 @@
+#include "machine.h"
+
+void load_program(Machine *pmach,
+                  unsigned textsize, Instruction text[textsize],
+                  unsigned datasize, Word data[datasize],  unsigned dataend)
+{
+	
+}
+
+void read_program(Machine *mach, const char *programfile)
+{
+
+}
+
+void dump_memory(Machine *pmach)
+{
+
+}
+
+void print_program(Machine *pmach)
+{
+
+}
+
+void print_data(Machine *pmach)
+{
+
+}
+
+void print_cpu(Machine *pmach)
+{
+
+}
+
+void simul(Machine *pmach, bool debug)
+{
+
+}
